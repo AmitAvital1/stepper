24d7d9e03ce9106c9f8137a7b1ec6ab021e4a5f2
Amit <amitav21@gmail.com>
2023-05-28 11:03:12 +0300

    Added formal output to execute page

--- stepper-ui-JavaFx/src/app/resources/body/execution/FlowsExecutionController.java
+++ stepper-ui-JavaFx/src/app/resources/body/execution/FlowsExecutionController.java
@@ -2,20 +2,26 @@ package app.resources.body.execution;
 
 import app.resources.body.BodyController;
 import app.resources.body.BodyControllerDefinition;
-import javafx.beans.property.IntegerProperty;
-import javafx.beans.property.ObjectProperty;
-import javafx.beans.property.SimpleIntegerProperty;
-import javafx.beans.property.SimpleObjectProperty;
+import app.resources.body.history.HistoryController;
+import javafx.beans.property.*;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
 import org.controlsfx.control.PopOver;
 import project.java.stepper.dd.impl.DataDefinitionRegistry;
 import project.java.stepper.dd.impl.list.ListData;
+import project.java.stepper.dd.impl.relation.RelationData;
 import project.java.stepper.exceptions.MissMandatoryInput;
 import project.java.stepper.exceptions.StepperExeption;
 import project.java.stepper.flow.definition.api.FlowDefinition;
@@ -33,6 +39,8 @@ import javafx.geometry.Pos;
 import javafx.animation.PauseTransition;
 
 import javafx.scene.text.Text;
+
+import java.awt.*;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
@@ -190,17 +198,11 @@ public class FlowsExecutionController implements BodyControllerDefinition {
         /*
         This function execute flow the user chose and validate all the free inputs has entered. then execute the flow.
          */
-        Accordion accordion = new Accordion();
-        TitledPane titledPane = new TitledPane("Execution Details", flowDetailsExecutionBox);
-        titledPane.setStyle("-fx-font-size: 16px;");
-        VBox currentParent = (VBox) flowDetailsExecutionBox.getParent();
-        ((Pane) currentParent).getChildren().clear();
-        titledPane.setContent(flowDetailsExecutionBox);
-        accordion.getPanes().add(titledPane);
-        currentParent.getChildren().addAll(accordion,flowExecutionInfo);
+        putInAccordionTheExecuteDetails();
         flowDetailsExecutionBox.setDisable(true);
         flowExecutionInfo.setVisible(true);
         stepResLabel.setText("");
+        formalOutPutsVbox.getChildren().clear();
         stepsProgressTreeView.setRoot(new TreeItem<>());
         System.out.println("Starting execution of flow " + flow.getFlowDefinition().getName() + " [ID: " + flow.getUniqueId() + "]");
         bodyForFlowExecutionController.getFlowManagerExecution().exeFlow(flow);
@@ -210,6 +212,7 @@ public class FlowsExecutionController implements BodyControllerDefinition {
         flow.getFlowExecutionResultProperty().addListener((observable, oldValue, newValue) -> {
             if(newValue != FlowExecutionResult.PROCESSING) {
                 stepResLabel.setText(newValue.name() + " [" + flow.getDuration() + ".ms" + "]");
+                addFormalOutputs(flow);
             }
         });
         flow.getFlowContexts().getFlowStepsDataProperty().addListener((observable, oldValue, newValue) -> {
@@ -235,24 +238,6 @@ public class FlowsExecutionController implements BodyControllerDefinition {
                     stepsProgressTreeView.getRoot().getChildren().addAll(stepNameItem);
                 }
             }
-//            Map<String,Object> formalOutputToData = flow.getFormalOutPutsData();
-//            for (Map.Entry<String, Object> entry : formalOutputToData.entrySet()) {
-//                Label outText;
-//                if(entry.getValue() == null)
-//                    break;
-//                if (entry.getValue().getClass() == ListData.class) {
-//                    outText = new Label(entry.getKey());
-//                    HBox hbox = new HBox();
-//                    hbox.setSpacing(5);
-//                    hbox.getChildren().add(outText);
-//                    Hyperlink hyperlink = new Hyperlink("Show list");
-//                    hyperlink.setOnAction(event -> showListPopup(entry.getValue()));
-//                    hbox.getChildren().add(hyperlink);
-//                    formalOutPutsVbox.getChildren().add(hbox);
-//                }
-//
-//
-//            }
         });
         //System.out.println("End execution of flow " + flow.getFlowDefinition().getName() + " [ID: " + flow.getUniqueId() + "]. Status: " + flow.getFlowExecutionResult());
       //  System.out.println("Outputs:");
@@ -261,6 +246,54 @@ public class FlowsExecutionController implements BodyControllerDefinition {
         //}
         bodyForFlowExecutionController.addFlowExecutor(flow);
     }
+
+    private void addFormalOutputs(FlowExecution flow) {
+        Map<String, Object> formalOutputToData = flow.getFormalOutPutsData();
+        Label Title = new Label("Formal outputs");
+        Title.setFont(javafx.scene.text.Font.font(Font.BOLD));
+        Title.setStyle("-fx-font-size: 14px;");
+        for (Map.Entry<String, Object> entry : formalOutputToData.entrySet()) {
+            Label outText;
+            if (entry.getValue() == null)
+                break;
+            if (entry.getValue().getClass() == ListData.class) {
+                outText = new Label(entry.getKey());
+                HBox hbox = new HBox();
+                hbox.setSpacing(5);
+                hbox.getChildren().add(outText);
+                Hyperlink hyperlink = new Hyperlink("Show list");
+                hyperlink.setOnAction(event -> showListPopup(entry.getValue()));
+                hbox.getChildren().add(hyperlink);
+                formalOutPutsVbox.getChildren().add(hbox);
+            } else if (entry.getValue().getClass() == RelationData.class) {
+                outText = new Label(entry.getKey());
+                HBox hbox = new HBox();
+                hbox.setSpacing(5);
+                hbox.getChildren().add(outText);
+                Hyperlink hyperlink = new Hyperlink("Show table");
+                hyperlink.setOnAction(event -> showTablePopup(entry.getValue()));
+                hbox.getChildren().add(hyperlink);
+                formalOutPutsVbox.getChildren().add(hbox);
+            }
+            else{
+                outText = new Label(entry.getKey() + " [" + entry.getValue() + "]");
+                formalOutPutsVbox.getChildren().add(outText);
+            }
+        }
+    }
+
+    private void putInAccordionTheExecuteDetails() {
+        Accordion accordion = new Accordion();
+        accordion.setPadding(new Insets(10,0,15,0));
+        TitledPane titledPane = new TitledPane("Execution Details", flowDetailsExecutionBox);
+        titledPane.setStyle("-fx-font-size: 16px;");
+        VBox currentParent = (VBox) flowDetailsExecutionBox.getParent();
+        ((Pane) currentParent).getChildren().clear();
+        titledPane.setContent(flowDetailsExecutionBox);
+        accordion.getPanes().add(titledPane);
+        currentParent.getChildren().addAll(accordion,flowExecutionInfo);
+    }
+
     private void showListPopup(Object flowData) {
         Stage popupStage = new Stage();
         ListView<String> tableView = new ListView<>();
@@ -270,5 +303,47 @@ public class FlowsExecutionController implements BodyControllerDefinition {
         Scene popupScene = new Scene(tableView);
         popupStage.setScene(popupScene);
         popupStage.show();
+    }
+    private void showTablePopup(Object flowData) {
+        Stage popupStage = new Stage();
+        TableView<ObservableList<FlowsExecutionController.StringPropertyWrapper>> tableView = new TableView<>();
+        //tableView.setColumnResizePolicy(FlowsExecutionController.getColumnResizePolicy());
+
+        ObservableList<ObservableList<FlowsExecutionController.StringPropertyWrapper>> data = FXCollections.observableArrayList();
+        RelationData table = (RelationData) flowData;
+
+        for (String columnName : table.getColumns()) {
+            final int columnIndex = table.getColumns().indexOf(columnName);
+            TableColumn<ObservableList<FlowsExecutionController.StringPropertyWrapper>, String> column = new TableColumn<>(columnName);
+            column.setCellValueFactory(cellData ->
+                    cellData.getValue().get(columnIndex).getValueProperty());
+
+            column.setCellFactory(TextFieldTableCell.forTableColumn());
+
+            tableView.getColumns().add(column);
+        }
+        tableView.setItems(data);
+
+        for(int i = 0; i < table.getRowsSize(); i++) {
+            ObservableList<FlowsExecutionController.StringPropertyWrapper> row = FXCollections.observableArrayList();
+            for (String value : table.getRowDataByColumnsOrder(i)) {
+                row.add(new FlowsExecutionController.StringPropertyWrapper(value));
+            }
+            data.add(row);
+        }
+        Scene popupScene = new Scene(tableView);
+        popupStage.setScene(popupScene);
+        popupStage.show();
+    }
+    private static class StringPropertyWrapper {
+        private final SimpleStringProperty valueProperty;
+
+        public StringPropertyWrapper(String value) {
+            this.valueProperty = new SimpleStringProperty(value);
+        }
+
+        public ObservableValue<String> getValueProperty() {
+            return valueProperty;
+        }
     }
 }
